2753dac49e3716e5bd53ba15c08f11ce23128d1c
anagvillanueva <anafernandagv27@gmail.com>
2020-09-23 22:24:49 -0500

    Prog1, repaso de while y do while

---
+++ ProgUno/src/fes/aragon/inicio/Inicio.java
@@ -0,0 +1,10 @@
+
+package fes.aragon.inicio;
+
+
+public class Inicio {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo ");
+    }
+    
+}
---
+++ ProgUno/src/fes/aragon/inicio/While.java
@@ -0,0 +1,13 @@
+
+package fes.aragon.inicio;
+
+
+public class While {
+    public static void main(String[] args) {
+        int i=0;
+        while (i<=10){
+            System.out.print(i+" ");
+            i++;
+        }
+    }
+}
