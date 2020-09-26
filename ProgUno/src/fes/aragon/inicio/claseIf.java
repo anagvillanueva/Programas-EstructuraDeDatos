4eff6a6d3630c7ab8bc92b9654b304f9abb52f32
anagvillanueva <anafernandagv27@gmail.com>
2020-09-25 23:40:35 -0500

    Este si es de if, el otro operadores ):

---
+++ ProgUno/src/fes/aragon/inicio/claseIf.java
@@ -0,0 +1,21 @@
+
+package fes.aragon.inicio;
+
+public class claseIf {
+    public static void main(String[] args) {
+        
+        boolean condicion;
+        
+        condicion = false;
+        
+        if(condicion == 10>8)
+        {
+            System.out.println("Cuando la verdad es verdadera");
+        }
+        else 
+        {
+            System.out.println("El juguito de tus mentiras");
+        }
+    }
+ 
+}
